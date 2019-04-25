bde6d14ab1954e420ae43c17d0d856f9c83845a5
Mahdi Nouri <m4hdi.pdroid@gmail.com>
2019-04-25 16:53:26 +0430

    Reformat Injector to have a more readable code

--- app/src/main/java/com/workshop/aroundme/app/Injector.kt
+++ app/src/main/java/com/workshop/aroundme/app/Injector.kt
@@ -17,33 +17,62 @@ import com.workshop.aroundme.remote.service.PlaceService
 
 object Injector {
 
+    private val networkManager = NetworkManager()
+
+    fun provideCategoryRepository(
+        categoryRemoteDataSource: CategoryRemoteDataSource = provideCategoryRemoteDataSource()
+    ): CategoryRepository {
+        return CategoryRepository(categoryRemoteDataSource)
+    }
+
+    private fun provideCategoryRemoteDataSource(
+        categoryService: CategoryService = provideCategoryService()
+    ): CategoryRemoteDataSource {
+        return CategoryRemoteDataSource(categoryService)
+    }
+
+    private fun provideCategoryService(): CategoryService {
+        return CategoryService(networkManager)
+    }
+
+    fun providePlaceRepository(
+        context: Context,
+        placeLocalDataSource: PlaceLocalDataSource = providePlaceLocalDataSource(context),
+        placeRemoteDataSource: PlaceRemoteDataSource = providePlaceRemoteDataSource()
+    ): PlaceRepository {
+        return PlaceRepository(placeLocalDataSource, placeRemoteDataSource)
+    }
+
+    private fun providePlaceLocalDataSource(context: Context): PlaceLocalDataSource {
+        return PlaceLocalDataSource(provideAppDatabase(context).placeDao())
+    }
+
+    private fun providePlaceRemoteDataSource(
+        placeService: PlaceService = providePlaceService()
+    ): PlaceRemoteDataSource {
+        return PlaceRemoteDataSource(placeService)
+    }
+
+    private fun providePlaceService(): PlaceService {
+        return PlaceService(networkManager)
+    }
+
+    fun provideUserRepository(
+        context: Context,
+        userLocalDataSource: UserLocalDataSource = provideUserLocalDataSource(context)
+    ): UserRepository {
+        return UserRepository(userLocalDataSource)
+    }
+
+    private fun provideUserLocalDataSource(
+        context: Context,
+        defaultSharedPreferences: SharedPreferences = provideDefaultSharedPref(context)
+    ): UserLocalDataSource {
+        return UserLocalDataSource(defaultSharedPreferences)
+    }
+
     private fun provideAppDatabase(context: Context): AppDatabase {
         return Room.databaseBuilder(context, AppDatabase::class.java, "db.data").build()
-    }
-
-    fun provideCategoryRepository(): CategoryRepository {
-        return CategoryRepository(CategoryRemoteDataSource(CategoryService(NetworkManager())))
-    }
-
-    fun providePlaceRepository(context: Context): PlaceRepository {
-        return PlaceRepository(
-            PlaceLocalDataSource(
-                provideAppDatabase(context).placeDao()
-            ),
-            PlaceRemoteDataSource(
-                PlaceService(
-                    NetworkManager()
-                )
-            )
-        )
-    }
-
-    fun provideUserRepository(context: Context): UserRepository {
-        return UserRepository(
-            UserLocalDataSource(
-                provideDefaultSharedPref(context)
-            )
-        )
     }
 
     private fun provideDefaultSharedPref(context: Context): SharedPreferences {
